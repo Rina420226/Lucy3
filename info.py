8de971979dffdfaa0d4a1f20cbf9e602da8de941
Rina420226 <rinamurmu231@gmail.com>
2026-04-10 06:35:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,9 +90,9 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/How_To_Download_OTx')
 # ============================
 # Link Shortener Configuration
 # ============================
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'nowshort.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e3b32859bdb46cc11255422458f8e433d8176911')
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrinkme.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', '79bfe9f22cb7a7177ad37f940cbc203ec8631610')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_nowshort')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
